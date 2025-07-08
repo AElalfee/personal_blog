5be08bd7671b7ace71ceb253beecba7135a4635c
Abdelrahman Elalef <abdelrahman@elalfee.com>
2025-07-08 21:25:15 +0400

    Add flask SQLAlchemy

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe(32))
     DEBUG = os.getenv("DEBUG", "True") == "True"
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///app.db")
