37adfd46b5dffd280c43c236a2dd762599a498e8
Abdelrahman Elalef <abdelrahman@elalfee.com>
2025-07-09 00:54:32 +0400

    render the rest crud operations

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, request, render_template
+from flask import Blueprint, flash, request, render_template, redirect, url_for
 
 from app.models import Blog, User
 
@@ -7,8 +7,10 @@ from . import db
 routes_bp = Blueprint("blogs", __name__)
 
 
-@routes_bp.route("/new", methods=["POST"])
+@routes_bp.route("/new", methods=["GET", "POST"])
 def create_blog():
+    if request.method == "GET":
+        return render_template("create_blog.html")
     user = User.query.first()
 
     blog = Blog(
@@ -23,7 +25,7 @@ def create_blog():
     data = blog.to_dict()
 
     flash("Blog created successfully", "success")
-    return data
+    return render_template("blog_details.html", blog=data)
 
 
 @routes_bp.route("/", methods=["GET"])
@@ -37,7 +39,7 @@ def get_blogs():
 def get_blog(blog_id):
     blog = Blog.query.get_or_404(blog_id)
     data = blog.to_dict()
-    return data
+    return render_template("blog_details.html", blog=data)
 
 
 @routes_bp.route("/delete/<int:blog_id>", methods=["POST"])
@@ -48,19 +50,19 @@ def delete_blog(blog_id):
     db.session.commit()
 
     flash("Blog deleted successfully", "success")
-    return
+    return redirect(url_for("blogs.get_blogs"))
 
 
-@routes_bp.route("/edit/<int:blog_id>", methods=["POST"])
+@routes_bp.route("/edit/<int:blog_id>", methods=["POST", "GET"])
 def update_blog(blog_id):
     blog = Blog.query.get_or_404(blog_id)
 
-    blog.title = request.form.get("title")
-    blog.content = request.form.get("content")
+    if request.method == "POST":
+        blog.title = request.form.get("title")
+        blog.content = request.form.get("content")
 
-    db.session.commit()
+        db.session.commit()
 
-    data = blog.to_dict()
-
-    flash("Blog updated successfully", "success")
-    return data
+        flash("Blog updated successfully", "success")
+        return redirect(url_for("blogs.get_blog", blog_id=blog.id))
+    return render_template("update_blog.html", blog=blog.to_dict())
